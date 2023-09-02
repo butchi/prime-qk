4add4f21ee8a82113f92cb9eb3d63ad481302225
butchi <butchiyu+com-github@gmail.com>
2023-09-02 10:50:39 +0900

    state周りリファクタリング

--- docs/script.js
+++ docs/script.js
@@ -7,19 +7,16 @@ const canSubmit = { value: false }
 
 const actionTarget = new EventTarget()
 
-const playerArr = ["A", "B", "you", "C"]
-
 const consoleElm = document.querySelector(".console")
 
 const cmdBoxElm = document.querySelector("[data-box-command]")
 
 const cmdInputElm = cmdBoxElm.querySelector("input")
 
-const deck = (new Array(13 * 4)).fill(0).map((_, i) => Math.floor(i / 4) + 1).concat([-1, -1])
-
-const handArr = []
-
-const stateGameObj = {
+const stateGameDefault = {
+    deck: [],
+    playerArr: ["A", "B", "you", "C"],
+    handArr: [],
     rev: false,
 }
 
@@ -101,160 +98,175 @@ const log = {
 }
 
 const execCommand = (inputStr = "") => {
-    const currentHand = handArr[state.turn.playerIdx]
+    const { game, set, turn } = state
+
+    const { deck, playerArr } = game
+    const { playerIdx } = turn
+
+    const hand = game.handArr[playerIdx]
 
     if (inputStr == null) {
     } else if (inputStr === "") {
-        handArr[state.turn.playerIdx]
+        handArr[playerIdx]
 
         return
     } else if (inputStr.toLowerCase() === "pass" || inputStr.toLowerCase() === "p") {
         log.popoutBq()
 
-        log.editCode(`${playerArr[state.turn.playerIdx]}: pass => ${Qk.fromArrayToString(currentHand)}`)
+        log.editCode(`${playerArr[playerIdx]}: pass => ${Qk.fromArrayToString(hand)}`)
 
-        state.set.prevPassCnt++
+        set.prevPassCnt++
 
         cmdInputElm.value = ""
 
         return inputStr
     } else if (inputStr.toLowerCase() === "draw" || inputStr.toLowerCase() === "d") {
-        log.popoutBq()
-
         cmdInputElm.value = ""
 
-        if (state.turn.draw) {
+        if (turn.draw) {
             return
         }
 
-        state.turn.draw = deck.shift()
+        log.popoutBq()
 
-        currentHand.push(state.turn.draw)
+        turn.draw = deck.shift()
 
-        Qk.sortArray(currentHand)
+        hand.push(turn.draw)
 
-        log.editCode(`${playerArr[state.turn.playerIdx]}: draw(${Qk.fromValToChar(state.turn.draw)}) => ${Qk.fromArrayToString(currentHand)}`)
+        Qk.sortArray(hand)
+
+        log.editCode(`${playerArr[playerIdx]}: draw(${Qk.fromValToChar(turn.draw)}) => ${Qk.fromArrayToString(hand)}`)
 
         cmdInputElm.value = ""
 
         log.bq("あなたの番です   p: pass")
-    }
-
-    const inputQkSeq = new QkCardSequence(inputStr)
-
-    const inputQkStr = inputQkSeq.toQkString()
-
-    const inputCardLen = inputQkSeq.getCardLength()
-
-    const inputQkArr = inputQkSeq.toQkArray()
-
-    const handTmp = [...currentHand]
-
-    const spliceIndexArr = inputQkArr.map(cardVal => {
-        const index = handTmp.findIndex(handVal => Qk.valueOf(handVal) === Qk.valueOf(cardVal))
-
-        if (index >= 0) {
-            handTmp.splice(index, 1)
-
-            return index
-        } else {
-            return NaN
-        }
-    })
-
-    if (spliceIndexArr.includes(NaN)) {
-        return
-    }
-
-    const inputNum = inputQkSeq.toQkNumber()
-    const inputBigInt = inputQkSeq.toQkBigInt()
-
-    const factorArr = factor(inputNum)
-
-    const isPrime = checkPrime(inputNum) || checkPrimeBigInt(inputBigInt)
-
-    const drawStr = state.turn.draw ? `draw(${Qk.fromValToChar(state.turn.draw)}) ` : ""
-    let prependHtml = `${playerArr[state.turn.playerIdx]}: ${drawStr}${inputQkStr}`
-    let attackHtml = ""
-
-    let isValid
-
-    if (inputNum == null) {
-    } else if (inputNum === Infinity) {
-        attackHtml += ` <i class="bi-check"></i> <span class="badge bg-secondary">Joker</span>`
-
-        isValid = true
-    } else if (inputNum === 57) {
-        attackHtml += ` <i class="bi-check"></i> <span class="badge bg-secondary">GC</span>`
-
-        isValid = true
-    } else if (inputNum === 1) {
-        attackHtml += ` <i class="bi-x-circle-fill"></i> <small>${inputNum} is not prime number</small>`
-
-        isValid = false
-    } else if (isFinite(inputNum)) {
-        if (isPrime) {
-            attackHtml += ` <i class="bi-check-circle-fill"></i> <small>${inputNum} is prime number</small>`
-
-            isValid = true
-        } else {
-            attackHtml += ` <i class="bi-x-circle-fill"></i> <small>${inputNum} = ${factorArr.join(" × ")}</small>`
-
-            const checkArr = [2, 3, 5, 11, 1001]
-
-            checkArr.forEach(num => {
-                if (inputNum % num === 0) {
-                    attackHtml += ` <span class="badge bg-secondary">${num}n</span>`
-                }
-            })
-
-            isValid = false
-        }
-    } else if (!isPrime) {
-        attackHtml += ` <i class="bi-x-circle-fill"></i> <small>${inputBigInt} is not prime number</small>`
-
-        isValid = false
     } else {
-        return
-    }
+        const inputQkSeq = new QkCardSequence(inputStr)
 
-    if (isValid) {
-        spliceIndexArr.forEach(idx => {
-            const spliceArr = currentHand.splice(idx, 1)
-
-            if (spliceArr && spliceArr.length === 1) {
-                deck.push(spliceArr[0])
+        const inputQkStr = inputQkSeq.toQkString()
+    
+        const inputCardLen = inputQkSeq.getCardLength()
+    
+        const inputQkArr = inputQkSeq.toQkArray()
+    
+        const handTmp = [...hand]
+    
+        const spliceIndexArr = inputQkArr.map(cardVal => {
+            const index = handTmp.findIndex(handVal => Qk.valueOf(handVal) === Qk.valueOf(cardVal))
+    
+            if (index >= 0) {
+                handTmp.splice(index, 1)
+    
+                return index
+            } else {
+                return NaN
             }
         })
-    } else {
-        for (let i = 0; i < inputCardLen; i++) {
-            const val = deck.shift()
-
-            if (val) {
-                currentHand.push(val)
+    
+        if (spliceIndexArr.includes(NaN)) {
+            return
+        }
+    
+        const inputNum = inputQkSeq.toQkNumber()
+        const inputBigInt = inputQkSeq.toQkBigInt()
+    
+        const factorArr = factor(inputNum)
+    
+        const isPrime = checkPrime(inputNum) || checkPrimeBigInt(inputBigInt)
+    
+        const drawStr = turn.draw ? `draw(${Qk.fromValToChar(turn.draw)}) ` : ""
+        let prependHtml = `${playerArr[playerIdx]}: ${drawStr}${inputQkStr}`
+        let attackHtml = ""
+    
+        let isValid
+    
+        if (inputNum == null) {
+        } else if (inputNum === Infinity) {
+            attackHtml += ` <i class="bi-check"></i> <span class="badge bg-secondary">Joker</span>`
+    
+            isValid = true
+        } else if (inputNum === 57) {
+            attackHtml += ` <i class="bi-check"></i> <span class="badge bg-secondary">GC</span>`
+    
+            isValid = true
+        } else if (inputNum === 1) {
+            attackHtml += ` <i class="bi-x-circle-fill"></i> <small>${inputNum} is not prime number</small>`
+    
+            isValid = false
+        } else if (isFinite(inputNum)) {
+            if (isPrime) {
+                attackHtml += ` <i class="bi-check-circle-fill"></i> <small>${inputNum} is prime number</small>`
+    
+                isValid = true
+            } else {
+                attackHtml += ` <i class="bi-x-circle-fill"></i> <small>${inputNum} = ${factorArr.join(" × ")}</small>`
+    
+                const checkArr = [2, 3, 5, 11, 1001]
+    
+                checkArr.forEach(num => {
+                    if (inputNum % num === 0) {
+                        attackHtml += ` <span class="badge bg-secondary">${num}n</span>`
+                    }
+                })
+    
+                isValid = false
+            }
+        } else if (!isPrime) {
+            attackHtml += ` <i class="bi-x-circle-fill"></i> <small>${inputBigInt} is not prime number</small>`
+    
+            isValid = false
+        } else {
+            return
+        }
+    
+        if (isValid) {
+            spliceIndexArr.forEach(idx => {
+                const spliceArr = hand.splice(idx, 1)
+    
+                if (spliceArr && spliceArr.length === 1) {
+                    deck.push(spliceArr[0])
+                }
+            })
+        } else {
+            for (let i = 0; i < inputCardLen; i++) {
+                const val = deck.shift()
+    
+                if (val) {
+                    hand.push(val)
+                }
             }
         }
+
+        log.editCode(`${prependHtml} => ${Qk.fromArrayToString(hand)} ${attackHtml}`)
+
+        Qk.sortArray(hand)
+
+        if (hand.length === 0) {
+            log.bq("You win!")
+        }
+
+        set.prevPassCnt = 0
+
+        log.popoutBq()
+
+        cmdInputElm.value = ""
+    
+        return inputStr
     }
-
-    log.editCode(`${prependHtml} => ${Qk.fromArrayToString(currentHand)} ${attackHtml}`)
-
-    Qk.sortArray(currentHand)
-
-    if (currentHand.length === 0) {
-        log.bq("You win!")
-    }
-
-    state.set.prevPassCnt = 0
-
-    cmdInputElm.value = ""
-
-    return inputStr
 }
 
 const startGame = _ => {
     log.h2(`Prime QK at ${new Date().toISOString()}`)
 
-    deck.sort(_ => Math.random() - .5)
+    state.game = { ...stateGameDefault }
+
+    state.game.deck = (new Array(13 * 4)).fill(0).map((_, i) => Math.floor(i / 4) + 1).concat([-1, -1])
+
+    const { game } = state
+
+    const { deck, playerArr, handArr } = game
+
+    game.deck.sort(_ => Math.random() - 0.5)
 
     log.h3("new game")
 
@@ -272,8 +284,12 @@ const startSet = async _ => {
 
     state.set = { ...stateSetDefault }
 
+    const { game, set } = state
+
+    const { playerArr } = game
+
     for (let i = 0; i < 99999; i++) {
-        if (state.set.prevPassCnt >= playerArr.length) {
+        if (set.prevPassCnt >= playerArr.length) {
             startSet()
 
             break
@@ -290,9 +306,11 @@ const startTurn = async idx => {
 
     state.turn = { ...stateTurnDefault, playerIdx: idx }
 
-    log.code(`${playerArr[idx]}: ${Qk.fromArrayToString(handArr[idx])}`)
+    const { game, turn } = state
 
-    const name = playerArr[idx]
+    log.code(`${game.playerArr[idx]}: ${Qk.fromArrayToString(game.handArr[idx])}`)
+
+    const name = game.playerArr[idx]
 
     if (name === "you") {
         canSubmit.value = true
