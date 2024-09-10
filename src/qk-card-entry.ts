0f1acf554ff35a6da985ef04cd95c3249539fa73
IWABUCHI Yuki <butchiyu+com-github@gmail.com>
2024-09-10 23:59:28 +0900

    ジョーカー処理実装

--- src/qk-card-entry.ts
+++ src/qk-card-entry.ts
@@ -16,9 +16,14 @@ export const cArrToGroup = (cardArr: QkCard[]): object => {
 
 export class QkCardEntry {
   private arr: QkCard[]
+  private jokerArr: string[]
 
-  private constructor(qkArr: QkCard[]) {
+  private constructor(qkArr: QkCard[], opts: { jokerArr: string[] } = { jokerArr: [] }) {
+    const { jokerArr } = opts
+
     this.arr = qkArr
+
+    this.jokerArr = jokerArr
   }
 
   add(qkCard: QkCard) {
@@ -34,6 +39,8 @@ export class QkCardEntry {
   }
 
   valueOf(): bigint | null {
+    let jokerIdx = 0
+
     const str: string = this.arr
       .map((card: QkCard) => {
         const val: number = card.valueOf() ?? NaN
@@ -41,6 +48,27 @@ export class QkCardEntry {
         if (isNaN(val)) {
           return null
         } else if (val === Infinity) {
+          if (this.toArray().length === 1) {
+            return null
+          }
+
+          const joker = this.jokerArr[jokerIdx++]
+
+          if (joker === undefined) {
+          } else if (joker === 'A') {
+            return '1'
+          } else if (joker === 'J') {
+            return '11'
+          } else if (joker === 'Q') {
+            return '12'
+          } else if (joker === 'K') {
+            return '13'
+          } else if (joker === 'T') {
+            return '10'
+          } else {
+            return joker
+          }
+
           console.warn('Set joker as 1 automatically')
           return '1'
         }
@@ -64,11 +92,21 @@ export class QkCardEntry {
 
   static from(str: string): QkCardEntry | null {
     try {
-      const qkCardArr: QkCard[] = str
+      const [numStr, expr1, expr2] = str.split('|')
+
+      const joker1 = /X=([0A2-9TJQK])/.exec(expr1)?.[1]
+
+      const joker2 = /X=([0A2-9TJQK])/.exec(expr2)?.[1]
+
+      const jokerArr = [joker1, joker2].filter(entry => entry !== undefined)
+
+      const qkCardArr: QkCard[] = numStr
         .split('')
         .map((char: string) => QkCard.from(char as QkCardChar))
 
-      return new QkCardEntry(qkCardArr)
+      return new QkCardEntry(qkCardArr, {
+        jokerArr,
+      })
     } catch (_err) {
       return null
     }
